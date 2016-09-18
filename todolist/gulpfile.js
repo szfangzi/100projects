6d7bd0de432f28fc211d434b1ee4e6a85c29eb73
fangzi <szfangziwei@qq.com>
2016-09-18 22:13:46 +0800

    test

---
+++ todolist/gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//命令行执行gulp 默认执行default任务
+gulp.task('default', function () {});
+
+gulp.task('sass', function () {
+  return gulp.src('src/public/css/*.scss')
+    .pipe(sass(/*{outputStyle: 'compressed'}*/).on('error', sass.logError))
+    .pipe(gulp.dest('src/public/css/'));
+});
+
+//监听文件改动
+gulp.task('watch', function () {
+  gulp.watch('src/public/css/*.scss', ['sass']);
+});
